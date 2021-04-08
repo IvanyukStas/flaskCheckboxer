a8ef00cb0ce686fb2a1008a6704ff5c862437b03
qqq <aswd25@yandex.ru>
2021-04-08 12:05:46 +0300

    Добавил файл для тестов

--- tests.py
+++ tests.py
@@ -1,4 +1,30 @@
+import unittest
 from checkboxer.models import Users
 from unittest import TestCase
+from config import Config
+from checkboxer import app, db
 
-class Tests(TestCase):
+
+
+
+
+class UserModelTests(TestCase):
+    def setUp(self) -> None:
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://'
+        db.create_all()
+
+
+    def tearDown(self) -> None:
+        db.session.remove()
+        db.drop_all()
+
+
+    def test_password_hashing(self):
+        u = Users(user_name='test')
+        u.set_password('`123456')
+        self.assertFalse(u.check_assword('asdf'))
+        self.assertTrue((u.check_assword('`123456')))
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
